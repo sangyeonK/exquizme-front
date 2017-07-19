4ff5d6addded23b5e9928562f7be15eeee5aaf1c
sangyeonK <sovietist@hotmail.com>
2017-07-20 01:27:18 +0900

    feat: 메인화면 구성 ( top-menu, main-page )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopMenuComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule
