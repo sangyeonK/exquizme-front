c456ba065384776b1d5ae677dd2da4790c1ec434
sangyeonK <sovietist@hotmail.com>
2017-07-30 12:21:25 +0900

    feat : RouterModule 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
@@ -7,6 +8,13 @@ import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SelectQuizComponent } from './pages/select-quiz/select-quiz.component';
 import { MiniconComponent } from './minicon/minicon.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'select_quiz', component: SelectQuizComponent },
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +26,8 @@ import { MiniconComponent } from './minicon/minicon.component';
     MiniconComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
