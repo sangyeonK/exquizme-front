aa522349cac6e8ca6e289aa917ed0797b72dc2a7
sangyeonK <sovietist@hotmail.com>
2017-08-02 02:32:18 +0900

    feat : 팝업기능 구현 - 활용성 연구 필요

--- src/app/pages/select-quiz/select-quiz.component.ts
+++ src/app/pages/select-quiz/select-quiz.component.ts
@@ -1,5 +1,10 @@
+/// <reference types="jquery"/>
 import { Component, OnInit } from '@angular/core';
 import { Quiz } from './model';
+
+interface JQueryX extends JQuery {
+  popup(any: any);
+}
 
 @Component({
   selector: 'app-select-quiz',
@@ -8,17 +13,31 @@ import { Quiz } from './model';
 })
 export class SelectQuizComponent implements OnInit {
 
-  quizzes : Quiz[];
+  quizzes: Quiz[];
 
-  constructor() { 
+  constructor() {
     this.quizzes = [];
     // TEST DATA
     this.quizzes.push(new Quiz(-1, "탕탕탕 FINGERTIP 네 맘을 겨눌게~"));
     this.quizzes.push(new Quiz(-1, "탕탕탕 FINGERTIP 심장이 멈추게~"));
+    this.quizzes.push(new Quiz(-1, "Shimmie Shimmie Ko Ko Bop"));
+    this.quizzes.push(new Quiz(-1, "Shimmie Shimmie Ko Ko Bop"));
     //...
   }
 
   ngOnInit() {
   }
 
+  quizDetail(event: Event, index: number) {
+
+    let target: Element = event.srcElement;
+    if (target) {
+      let currentQuiz: Quiz = this.quizzes[index];
+      (<JQueryX>$(target.parentElement)).popup({
+        title: `${currentQuiz.title.substr(0,15)}...`,
+        on: "click"
+      });
+    }
+
+  }
 }
