6ddd5f84c58ebc92c2b5c25ffaf1d5997651d549
sangyeonK <sovietist@hotmail.com>
2017-08-25 14:13:12 +0900

    feat : 외부URL 링크로 들어올때 code값에 따라 분기

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { environment } from '../environments/environment';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   env = environment;
   title = 'ExquizME';
+  sub: Subscription;
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private router: Router) {
 
+  }
+
+  ngOnInit() {
+    this.sub = this.route.queryParams.subscribe((params: Params) => {
+      if(params['code'])
+        this.router.navigate(['start_solve_quiz']);
+    })
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
